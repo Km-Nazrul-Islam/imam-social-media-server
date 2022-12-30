e109f9c3f846cf0710cbfdc34a805f8afcb602f6
nazrul-islam <nazrulislamrongon@gmail.com>
2022-12-30 23:29:15 +0600

    env file added

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async(req, res) => {
+		res.send('Social Meadia Server is Running')
+	})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p3lfnp2.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
+
+
+app.listen(port, () => console.log(`Social Media Running on ${port}`))
